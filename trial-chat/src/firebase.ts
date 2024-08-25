c3cb375bf07a05dad2a1620cc211ce90f119ca9b
watanabe-tsubasa <t.watanabe423@gmail.com>
2024-08-25 13:02:18 +0900

    upd

--- trial-chat/src/firebase.ts
+++ trial-chat/src/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Firebase の設定情報
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAQi8PzqthXunK_uBmJHQdQKLg1PRtVh7U",
+  authDomain: "first-trial-by-chat.firebaseapp.com",
+  projectId: "first-trial-by-chat",
+  storageBucket: "first-trial-by-chat.appspot.com",
+  messagingSenderId: "684952261836",
+  appId: "1:684952261836:web:a90e6c56e647a98c989e69"
 };
 
 // Firebase アプリの初期化
